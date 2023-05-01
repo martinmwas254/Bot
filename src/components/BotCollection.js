66cccdd288ca796980e643ff0fe4068415e4e37c
martinmwas254 <martinmwas264@gmail.com>
2023-05-01 15:01:36 +0300

    Adjust font size an add icons to bot card

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import "./BotCollection.css";
 import BotArmy from "./YourBotArmy";
 const BotCollection = () => {
   const [bots, setBots] = useState([]);
@@ -57,11 +57,15 @@ const BotCollection = () => {
               style={{ width: "180px" }}
             />
             <h3>{bot.name}</h3>
-            <h4>{bot.catchphrase}</h4>
+            <h4 style={{fontSize:"xx-small"}}>{bot.catchphrase}</h4>
             <h4>Class: {bot.bot_class}</h4>
-            <h4>Health: {bot.health}</h4>
-            <h4>Damage: {bot.damage}</h4>
-            <h4>Armor: {bot.armor}</h4>
+            
+            <div style={{display:'flex', gap:"8px"}}>
+           
+            <h4><img src="https://img.icons8.com/ios/50/null/heart-with-pulse--v1.png" style={{width:"22px"}}/>{bot.health}</h4>
+            <h4><img src="https://img.icons8.com/ios/50/null/gun.png" style={{width:"22px"}}/>{bot.damage}</h4>
+            <h4><img src="https://img.icons8.com/external-anggara-glyph-anggara-putra/32/null/external-sheild-ecommerce-anggara-glyph-anggara-putra.png" style={{width:"22px"}}/>{bot.armor}</h4>
+            </div>
             <button onClick={() => deleteBot(bot.id)} className="delete-button">
               X
             </button>
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -12,12 +12,14 @@ export default function BotArmy({ botArmy, removeBot }) {
               style={{ width: "180px" }}
             />
             <h3>{bot.name}</h3>
-            <h4>{bot.catchphrase}</h4>
-            <h4>Class: {bot.bot_class}</h4>
-            <h4>Health: {bot.health}</h4>
-            <h4>Damage: {bot.damage}</h4>
-            <h4>Armor: {bot.armor}</h4>
-            <button onClick={() => removeBot(bot)}>Remove</button>
+            <p style={{fontSize:"xx-small"}}>{bot.catchphrase}</p>
+            <h4>Class:{bot.bot_class}</h4>
+            <div style={{display:'flex',gap:"7px"}}>
+           
+            <h4><img src="https://img.icons8.com/ios/50/null/heart-with-pulse--v1.png" style={{width:"22px"}}/>{bot.health}</h4>
+            <h4><img src="https://img.icons8.com/ios/50/null/gun.png" style={{width:"22px"}}/>{bot.damage}</h4>
+            <h4><img src="https://img.icons8.com/external-anggara-glyph-anggara-putra/32/null/external-sheild-ecommerce-anggara-glyph-anggara-putra.png" style={{width:"22px"}}/>{bot.armor}</h4>
+            </div>
           </div>
         ))}
       </div>
